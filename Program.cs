4673b1976f8fffaaec98cd1d4c87b2dd415dce98
Sidney François <sidney.fvm.dev@outlook.com>
2023-02-25 19:59:06 -0300

    feat: add Employee class

--- Program.cs
+++ Program.cs
@@ -11,5 +11,18 @@ public class CSVProcessing
             HasHeaderRecord = true,
             IgnoreBlankLines = true,
         };
+
+
     }
+}
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double HourValue { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime EntryDate { get; set; }
+    public DateTime ExitTime { get; set; }
+    public List<DateTime> LunchTime { get; set; }
 }
