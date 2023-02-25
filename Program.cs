c144eaf5205c4a94c0a763227458f3bba54a1b31
Sidney François <sidney.fvm.dev@outlook.com>
2023-02-25 19:54:43 -0300

    feat: add CsvHelper initial configuration

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Globalization;
+using CsvHelper.Configuration;
+
+public class CSVProcessing
+{
+    public void Reading(string filename)
+    {
+        var configCsvHelper = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ";",
+            HasHeaderRecord = true,
+            IgnoreBlankLines = true,
+        };
+    }
+}
